6ba03bccf6710c02fcecd856c1282acce2cbc162
Zeouterlimits <gavinfitzgerald@gmail.com>
2015-02-14 16:56:46 +0000

    Added background music

---
+++ CupidsLineup/Assets/scripts/PersistAudio.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistAudio : MonoBehaviour {
+
+	private static PersistAudio instance = null;
+
+	public static PersistAudio Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
