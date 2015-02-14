8908f101a45a205fa8ce1662280efd38c61c7a60
Meghan Halton <meghalton@gmail.com>
2015-02-14 17:33:53 +0000

    Add main menu
    Add a start button on a screen.
    
    Signed-off-by: Meghan Halton <meghalton@gmail.com>

---
+++ CupidsLineup/Assets/scripts/MainMenuButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void onClick() {
+		Application.LoadLevel("01_girl_description");
+	}
+}
