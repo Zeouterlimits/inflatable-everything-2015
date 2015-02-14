e2df1edf7e548fc9384fbc9c5240fd569d92a43f
Zeouterlimits <gavinfitzgerald@gmail.com>
2015-02-14 13:54:15 +0000

    Genericify images for less specific name code

--- CupidsLineup/Assets/scripts/GirlSpriteBuilder.cs
+++ CupidsLineup/Assets/scripts/GirlSpriteBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GirlSpriteBuilder : MonoBehaviour {
 
@@ -13,6 +14,7 @@ public class GirlSpriteBuilder : MonoBehaviour {
 	Transform childHead;
 	Transform childOther;
 
+
 	// Use this for initialization
 	void Start () {
 		childHead = transform.Find("Head");
@@ -25,30 +27,17 @@ public class GirlSpriteBuilder : MonoBehaviour {
 
 	void setCorrectSprites ()
 	{
-		if (chosenHead == 1) {
-			Sprite headSprite = Resources.Load ("head_normal_eyes", typeof(Sprite)) as Sprite;	
+			Sprite headSprite = Resources.Load ("head_" + chosenHead, typeof(Sprite)) as Sprite;	
 			childHead.GetComponent<SpriteRenderer> ().sprite = headSprite;
-			//childHead.localScale = new Vector3( 5f, 5f, 5f);
-		}
-		if (chosenBody == 1) {
-			//childBody.position += Vector3.down * 15f;
-			Sprite bodySprite = Resources.Load ("basic_tshirt_with_arms", typeof(Sprite)) as Sprite;
-			childBody.GetComponent<SpriteRenderer> ().sprite = bodySprite;
-			//childBody.localScale = new Vector3( 5f, 5f, 5f);
-		}
 
-		if (chosenOther == 1) {
-			//childLegs.position += Vector3.down * 40f;
-			Sprite accessorySprite = Resources.Load ("basic_accesorises_balloons", typeof(Sprite)) as Sprite;
+			Sprite bodySprite = Resources.Load ("body_" + chosenBody, typeof(Sprite)) as Sprite;
+			childBody.GetComponent<SpriteRenderer> ().sprite = bodySprite;
+
+			Sprite accessorySprite = Resources.Load ("other_" + chosenOther, typeof(Sprite)) as Sprite;
 			childOther.GetComponent<SpriteRenderer> ().sprite = accessorySprite;
-			//childLegs.localScale = new Vector3( 5f, 5f, 5f);
-		}
-		if (chosenLegs == 1) {
-			//childLegs.position += Vector3.down * 40f;
-			Sprite legSprite = Resources.Load ("basic_pants", typeof(Sprite)) as Sprite;
+
+			Sprite legSprite = Resources.Load ("legs_" + chosenLegs, typeof(Sprite)) as Sprite;
 			childLegs.GetComponent<SpriteRenderer> ().sprite = legSprite;
-			//childLegs.localScale = new Vector3( 5f, 5f, 5f);
-		}	
 	}
 	
 	// Update is called once per frame
