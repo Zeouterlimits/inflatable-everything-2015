3f843f9870cd3a0a80dc4f320ce327fb496beb0f
Zeouterlimits <gavinfitzgerald@gmail.com>
2015-02-14 10:06:21 +0000

    Added two scenes

---
+++ CupidsLineup/Assets/scripts/SceneManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WaitForSeconds(5);
+		Application.LoadLevel("02_lineup");
+	}
+}
