7397f452b19810580d23b9723f648c15928d12ec
Meghan Halton <meghalton@gmail.com>
2015-03-08 12:58:46 +0000

    Person: Make Person object serialisable
    Make it possible to serialise the Person object to/from XML.
    
    Signed-off-by: Meghan Halton <meghalton@gmail.com>

--- CupidsLineup/Assets/scripts/Girl/Person.cs
+++ CupidsLineup/Assets/scripts/Girl/Person.cs
@@ -1,14 +1,22 @@
 using System;
+using System.Xml.Serialization;
 using UnityEngine;
 
 		public class Person
 		{
+			[XmlAttribute("personName")]
 			public String personName;
+			[XmlElement("head")]
 			public int head = 0;
+			[XmlElement("body")]
 			public int body = 0;
+			[XmlElement("legs")]
 			public int legs = 0;
+			[XmlElement("other")]
 			public int other = 0;
+			[XmlElement("description")]
 			public String description;
+			[XmlElement("feedItems")]
 			public String[] feedItems;
 		}
 
